474992d2ba9517616e91f471d8a2eb867a51ae16
Selva08 <59572071+selva08@users.noreply.github.com>
2020-02-03 01:19:47 +0400

    Updates

---
+++ precache-manifest.785f987711ae05992b76f9c87a32b055.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f4b38ef690a1c2fed5976d723fe25df",
+    "url": "/smart-brain/index.html"
+  },
+  {
+    "revision": "971fd35a22298e3a2784",
+    "url": "/smart-brain/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "e292d43e729624b47ce4",
+    "url": "/smart-brain/static/css/main.8c8de799.chunk.css"
+  },
+  {
+    "revision": "971fd35a22298e3a2784",
+    "url": "/smart-brain/static/js/2.14f3c98b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/smart-brain/static/js/2.14f3c98b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e292d43e729624b47ce4",
+    "url": "/smart-brain/static/js/main.72845fa7.chunk.js"
+  },
+  {
+    "revision": "5dad3ccf3096ed8e3acd",
+    "url": "/smart-brain/static/js/runtime-main.b4947a8e.js"
+  }
+]);
